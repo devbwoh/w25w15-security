a4f31539494d9c722edc5721cb73f02f408d83a6
BWOH <bwoh@outlook.kr>
2025-12-09 10:01:58 +0900

    🔒️SameSite String 적용

--- src/main/kotlin/kr/ac/kumoh/s20250000/w25w15_security/controller/AuthController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20250000/w25w15_security/controller/AuthController.kt
@@ -1,11 +1,12 @@
 package kr.ac.kumoh.s20250000.w25w15_security.controller
 
-import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import kr.ac.kumoh.s20250000.w25w15_security.service.UserService
 import kr.ac.kumoh.s20250000.w25w15_security.util.JwtUtil
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseCookie
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.time.Duration
@@ -26,17 +27,27 @@ class AuthController(
         val user = userService.saveUser(username, password)
         val token = jwtUtil.generateToken(username)
 
-        val cookie = Cookie("accessToken", token).apply {
-            isHttpOnly = true
+        val cookie = ResponseCookie.from("accessToken", token)
+            .httpOnly(true)
+            .secure(false) // TODO: HTTPS를 사용하고 secure를 true로 변경할 것
+            .path("/")
+            .maxAge(Duration.ofMillis(20 * 1000L)) // 20초
+            //.maxAge(Duration.ofMinutes(30)) // 30분
+            .sameSite("Strict")
+            .build()
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString())
 
-            // TODO: HTTPS를 사용하고 secure를 true로 변경할 것
-            secure = false
+        // Refresh Token도 사용하면 더 편함
+//            val refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
+//                .httpOnly(true)
+//                .secure(false) // TODO: HTTPS를 사용하고 secure를 true로 변경할 것
+//                .path("/api/refresh")
+//                .maxAge(Duration.ofMillis(40 * 1000L)) // 40초
+//                //.maxAge(Duration.ofDays(15)) // 15일
+//                .sameSite("Strict")
+//                .build()
+//            response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
 
-            path = "/"
-            maxAge = (Duration.ofMillis(20 * 1000L).toSeconds()).toInt() // 20초
-            //maxAge = (Duration.ofMillis(60 * 60 * 1000L).toSeconds()).toInt() // 1시간
-        }
-        response.addCookie(cookie)
         return ResponseEntity.ok("회원 가입 성공")
     }
 
@@ -49,17 +60,27 @@ class AuthController(
         if (userService.authenticate(username, password)) {
             val token = jwtUtil.generateToken(username)
 
-            val cookie = Cookie("accessToken", token).apply {
-                isHttpOnly = true
+            val cookie = ResponseCookie.from("accessToken", token)
+                .httpOnly(true)
+                .secure(false) // TODO: HTTPS를 사용하고 secure를 true로 변경할 것
+                .path("/")
+                .maxAge(Duration.ofMillis(20 * 1000L)) // 20초
+                //.maxAge(Duration.ofMinutes(30)) // 30분
+                .sameSite("Strict")
+                .build()
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString())
 
-                // TODO: HTTPS를 사용하고 secure를 true로 변경할 것
-                secure = false
+            // Refresh Token도 사용하면 더 편함
+//            val refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
+//                .httpOnly(true)
+//                .secure(false) // TODO: HTTPS를 사용하고 secure를 true로 변경할 것
+//                .path("/api/refresh")
+//                .maxAge(Duration.ofMillis(40 * 1000L)) // 40초
+//                //.maxAge(Duration.ofDays(15)) // 15일
+//                .sameSite("Strict")
+//                .build()
+//            response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
 
-                path = "/"
-                maxAge = (Duration.ofMillis(20 * 1000L).toSeconds()).toInt() // 20초
-                //maxAge = (Duration.ofMillis(60 * 60 * 1000L).toSeconds()).toInt() // 1시간
-            }
-            response.addCookie(cookie)
             return ResponseEntity.ok("로그인 성공")
         } else {
             return ResponseEntity
